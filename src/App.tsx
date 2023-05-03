fef2200329e35ddb6ac499e8e317ed4414cdd755
lucas-possato-dev <lucaspstinho@gmail.com>
2023-05-03 13:22:54 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,99 @@
+import React, { useCallback } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { Keyboard } from "./components/Keyboard";
+import styles from "./App.module.css";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = React.useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = React.useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+
+  const isLoser = incorrectLetters.length >= 6;
+  const isWinner = wordToGuess
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
+  const addGuessedLetter = React.useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+      setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetters, isWinner, isLoser]
+  );
+
+  React.useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (!key.match(/^[a-z]$/)) return;
+
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+
+    document.addEventListener("keypress", handler);
+
+    return () => {
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetters]);
+
+  const handleClick = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && (
+          <>
+            Winner!{" "}
+            <button className={styles.btnTry} onClick={handleClick}>
+              Try Again
+            </button>
+          </>
+        )}
+        {isLoser && (
+          <>
+            Nice try!{" "}
+            <button className={styles.btnTry} onClick={handleClick}>
+              Try Again
+            </button>
+          </>
+        )}
+      </div>
+
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
